19ce45e7bcedd16a289e53214cda64317394a675
reetipd <reetipd@gmail.com>
2020-10-09 20:56:59 +0545

    Useprofile changes

--- minor/urls.py
+++ minor/urls.py
@@ -8,11 +8,7 @@ urlpatterns = [
     path('', include('jobrec.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-<<<<<<< HEAD
-] 
-=======
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
->>>>>>> db34ad5806e1102e071e82ac2d2df294b61ac44c
