48fc556091f96bf46933895026c43d9349ca7dce
reetipd <reetipd@gmail.com>
2020-10-09 20:56:21 +0545

    Useprofile changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,3 @@ admin.site.register(User)
 admin.site.register(Jobseeker)
 admin.site.register(Jobcreator)
 admin.site.register(jobrec)
-<<<<<<< HEAD
-=======
-
->>>>>>> db34ad5806e1102e071e82ac2d2df294b61ac44c
